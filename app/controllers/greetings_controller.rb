539ed8519e10a77d145766ed1e143b17cbe5568e
Raghu Betina <rbetina@gmail.com>
2013-02-20 14:51:18 -0600

    Added our first cookie.

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,10 @@
 class GreetingsController < ApplicationController
   def hello
+    @name = cookies[:visitor]
   end
 
   def introduce
+    cookies[:visitor] = 'Somebody'
+    redirect_to '/hi'
   end
 end
