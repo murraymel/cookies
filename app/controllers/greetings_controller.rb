3153f3d13c2621bc94a1db266f24e7ac74c3c6fd
Raghu Betina <rbetina@gmail.com>
2013-02-20 15:52:43 -0600

    Captured new form inputs in additional cookies. Prepared instance variables for use in view.

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,10 +1,14 @@
 class GreetingsController < ApplicationController
   def hello
     @name = cookies[:visitor]
+    @color = cookies[:color]
+    @size = cookies[:size]
   end
 
   def introduce
     cookies[:visitor] = params[:person]
+    cookies[:color] = params[:color]
+    cookies[:size] = params[:size]
     redirect_to '/hi'
   end
 
